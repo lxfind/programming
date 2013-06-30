4a2ecc10ab51e23cb18300bffb52ec91ec747a74
Xun Li <lxfind@gmail.com>
2013-06-30 11:30:41 -0700

    CodeChef Lunch Time June 2013

---
+++ Codechef/LTIME01/NUMFACT.cpp
@@ -0,0 +1,200 @@
+/*
+ * CodeChef Lunch Time June 2013
+ * Problem C: Number of Factors
+ * Algorithm: Factorization
+ * Copyright (C) 2013, Xun Li (lxfind@gmail.com)
+ */
+
+#include <set>
+#include <bitset>
+#include <list>
+#include <map>
+#include <queue>
+#include <stack>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <climits>
+#include <cstring>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#define MID(x,y) ( ( x + y ) >> 1 )
+#define L(x) ( x << 1 )
+#define R(x) ( x << 1 | 1 )
+#define REP(i,t) for(int i=0; i<(t); i++)
+#define FOR(i,s,t) for(int i=(s); i<(t); i++)
+#define FORD(i,a,b) for (int i = (a); i >= (b); i--)
+#define FORL(i,s,t) for(L i=(s); i<(t); i++)
+#define BUG puts("here!!!")
+#define STOP system("pause")
+#define file_r(x) freopen(x, "r", stdin)
+#define file_w(x) freopen(x, "w", stdout)
+#define EPS 1e-6
+#define EQ(a, b) (fabs((a) - (b)) <= EPS)
+#define POS(a) ((a) >= EPS)
+#define NEG(a) ((a) <= -EPS)
+#define BG(a, b) ((a) - (b) >= EPS)
+#define LS(a, b) ((b) - (a) >= EPS)
+#define CLR(a, x) memset( a, x, sizeof( a ) )
+#define PI (atan(1.0) * 4)
+#define SQ(x) ((x) * (x))
+#define DIST(x1, y1, x2, y2) (sqrt(SQ((x1) - (x2)) + SQ((y1) - (y2))))
+#define mp(x, y) make_pair(x, y)
+#define LOG2(x) (log(x) / log(2))
+#define XX first.first
+#define XY first.second
+#define YX second.first
+#define YY second.second
+
+using namespace std;
+typedef unsigned long long ULL;
+typedef long long LL;
+typedef pair<double, double> Pd;
+typedef pair<int, int> Pi;
+typedef pair<LL, LL> Pl;
+
+static struct IO {
+        char tmp[1 << 10];
+ 
+        // fast input routines
+        char cur;
+ 
+//#define nextChar() (cur = getc_unlocked(stdin))
+//#define peekChar() (cur)
+        inline char nextChar() { return cur = getc_unlocked(stdin); }
+        inline char peekChar() { return cur; }
+ 
+        inline operator bool() { return peekChar(); }
+        inline static bool isBlank(char c) { return (c < '-' && c); }
+        inline bool skipBlanks() { while (isBlank(nextChar())); return peekChar() != 0; }
+ 
+        inline IO& operator >> (char & c) { c = nextChar(); return *this; }
+ 
+        inline IO& operator >> (char * buf) {
+                if (skipBlanks()) {
+                        if (peekChar()) {
+                                *(buf++) = peekChar();
+                                while (!isBlank(nextChar())) *(buf++) = peekChar();
+                        } *(buf++) = 0; } return *this; }
+ 
+        inline IO& operator >> (string & s) {
+                if (skipBlanks()) {     s.clear(); s += peekChar();
+                        while (!isBlank(nextChar())) s += peekChar(); }
+                return *this; }
+ 
+        inline IO& operator >> (double & d) { if ((*this) >> tmp) sscanf(tmp, "%lf", &d); return *this; }
+ 
+#define defineInFor(intType) \
+        inline IO& operator >>(intType & n) { \
+                if (skipBlanks()) { \
+                        int sign = +1; \
+                        if (peekChar() == '-') { \
+                                sign = -1; \
+                                n = nextChar() - '0'; \
+                        } else \
+                                n = peekChar() - '0'; \
+                        while (!isBlank(nextChar())) { \
+                                n += n + (n << 3) + peekChar() - 48; \
+                        } \
+                        n *= sign; \
+                } \
+                return *this; \
+        }
+ 
+defineInFor(int)
+defineInFor(unsigned int)
+defineInFor(long long)
+ 
+        // fast output routines
+ 
+//#define putChar(c) putc_unlocked((c), stdout)
+        inline void putChar(char c) { putc_unlocked(c, stdout); }
+        inline IO& operator << (char c) { putChar(c); return *this; }
+        inline IO& operator << (const char * s) { while (*s) putChar(*s++); return *this; }
+ 
+        inline IO& operator << (const string & s) { for (int i = 0; i < (int)s.size(); ++i) putChar(s[i]); return *this; }
+ 
+        char * toString(double d) { sprintf(tmp, "%lf%c", d, '\0'); return tmp; }
+        inline IO& operator << (double d) { return (*this) << toString(d); }
+ 
+ 
+#define defineOutFor(intType) \
+        inline char * toString(intType n) { \
+                char * p = (tmp + 30); \
+                if (n) { \
+                        bool isNeg = 0; \
+                        if (n < 0) isNeg = 1, n = -n; \
+                        while (n) \
+                                *--p = (n % 10) + '0', n /= 10; \
+                        if (isNeg) *--p = '-'; \
+                } else *--p = '0'; \
+                return p; \
+        } \
+        inline IO& operator << (intType n) { return (*this) << toString(n); }
+ 
+defineOutFor(int)
+defineOutFor(long long)
+ 
+#define endl ('\n')
+#define cout __io__
+#define cin __io__
+} __io__;
+
+/*************************
+ * Sieve of Eratosthenes *
+ *************************
+ * Find all prime numbers within SIZE.
+ * Return number of primes found
+ */
+#define SIZE 1000000
+#define MAX (int)(SIZE-3)/2
+#define testPrime(n) (n == 2 || ((n > 1) && (n & 1) && bset.test((n - 3) / 2)))
+ 
+int primes[MAX+1]; //array that stores the primes up to sqrt(SIZE)
+bitset<MAX+1> bset; //auxiliary bitset used to make the sieve
+
+int setPrimes() {
+    int i,j;
+    //we only have to get primes up to sqrt(SIZE)
+    for (i = 0; i * i <= SIZE; i++)
+      if (!bset.test(i))
+        for( j = i + 1; (2 * j + 1) * (2 * i + 3) <= SIZE; j++)
+          bset.set(((2*j+1)*(2*i+3)-3)/2); //setting all non-primes
+    primes[0] = 2; //store the first prime (that is 2)
+    for (i = 1, j = 0; j < MAX + 1; j++)
+       if (!bset.test(j))
+          primes[i++] = 2 * j + 3; //store the remaining odd primes    
+    return i;
+}
+
+int main() {
+  int t;
+  cin >> t;
+  int cnt = setPrimes();
+  while (t--) {
+    int n;
+    cin >> n;
+    map<int, int> ans;
+    while (n--) {
+      int x;
+      cin >> x;
+      int i = 0;
+      while (x >= primes[i]) {
+        while (x && x % primes[i] == 0) {
+          ans[primes[i]]++;
+          x /= primes[i];
+        }
+        i++;
+      }
+    }
+    LL res = 1;
+    for (map<int, int>::iterator itr = ans.begin(); itr != ans.end(); itr++)
+      res *= itr->second + 1;
+    cout << res << endl;
+  }
+  return 0;
+}
